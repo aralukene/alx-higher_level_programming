c5a966fb3ed05c5eeae0ab9620bbd6b78d94ae34
aralukene <59538075+aralukene@users.noreply.github.com>
2023-05-23 17:24:46 +0100

    Create 6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const todos = JSON.parse(body);
+  const completedTasks = {};
+
+  todos.forEach(todo => {
+    if (todo.completed) {
+      if (completedTasks[todo.userId]) {
+        completedTasks[todo.userId]++;
+      } else {
+        completedTasks[todo.userId] = 1;
+      }
+    }
+  });
+  console.log(completedTasks);
+});
